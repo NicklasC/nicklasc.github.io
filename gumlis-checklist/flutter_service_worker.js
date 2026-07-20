9676e212f324d7cb3b9ca0454b01504acd00e637
Nicklas Carlsson <ncasv92@hotmail.com>
2026-07-20 22:18:42 +0200

    Build Family read-only navigation pages

--- gumlis-checklist/flutter_service_worker.js
+++ gumlis-checklist/flutter_service_worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Replaced with the app archive content hash during deployment.
-const BUILD_VERSION = '47390c5221ace811';
+const BUILD_VERSION = '8b6e0ea311b9707e';
 const CACHE_PREFIX = 'gumli-';
 const CACHE_NAME = `${CACHE_PREFIX}${BUILD_VERSION}`;
 const RUNTIME_CDN_HOSTS = new Set([
