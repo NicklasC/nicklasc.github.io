eb228bac89642a8cbb440b4254d3e927eb8a2627
Nicklas Carlsson <ncasv92@hotmail.com>
2026-07-22 15:51:29 +0200

    Build Family completion and history

--- gumlis-checklist/flutter_service_worker.js
+++ gumlis-checklist/flutter_service_worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Replaced with the app archive content hash during deployment.
-const BUILD_VERSION = 'c13d41c5cb27621f';
+const BUILD_VERSION = '4d278d6183e2fba8';
 const CACHE_PREFIX = 'gumli-';
 const CACHE_NAME = `${CACHE_PREFIX}${BUILD_VERSION}`;
 const RUNTIME_CDN_HOSTS = new Set([
